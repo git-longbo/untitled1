7868dad12ad8488a2aefdc7a804396db95e35e99
long <wt2894417@163.com>
2022-06-30 16:45:38 +0800

    version-3

--- src/main/java/com/example/Git.java
+++ src/main/java/com/example/Git.java
@@ -4,6 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("git---1");
         System.out.println("git---2");
+        System.out.println("git---3");
 
     }
 }
